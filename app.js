7d3264b9fa169653acaa5d339bde404d9d62142d
GMarcell <grand1310marcell@gmail.com>
2021-10-23 14:50:45 +0700

    Update

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-function dropdownFunction(a) {
-    a.parentNode.getElementsByClassName('dropdown-content')[0].classList.toggle("show");
-    a.parentNode.getElementsByClassName('dropbtn')[0].classList.toggle("active");
-    a.parentNode.getElementsByClassName('arrow')[0].classList.toggle("flip");
-}
+const accordionButtons = document.querySelectorAll(".dropbtn");
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdown = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdown.length; i++) {
-            var openDropdown = dropdown[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+const addActiveClass = event => event.target.classList.toggle("active");
+
+accordionButtons.forEach(button => button.addEventListener("click", addActiveClass));
